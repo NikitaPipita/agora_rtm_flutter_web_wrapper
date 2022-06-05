3c3f8dd3f82ad150349231a71aba3453046fa2b1
NikitaPipita <artur.rejet.vseh@gmail.com>
2022-06-06 00:07:35 +0300

    Added agora_rtm_bundle

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'production',
     entry: './agora_module.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'agora_rtm_bundle.js',
         path: path.resolve(__dirname, './dist'),
     },
     devServer: {
