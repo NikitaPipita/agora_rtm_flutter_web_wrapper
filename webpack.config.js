66b9f6163bee00e6140b2bad7739cbfac6066d26
NikitaPipita <artur.rejet.vseh@gmail.com>
2022-06-05 17:04:59 +0300

    Changed entry file name

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './index.js',
+    entry: './agora_module.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './dist'),
